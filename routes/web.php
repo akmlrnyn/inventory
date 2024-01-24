289d150687cd67641b234924cb1d9f6271922483
akmlrnyn <akmalranyan@gmail.com>
2024-01-24 09:36:09 +0700

    made user route and controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware('auth')->group(function () {
+    Route::resource('user', UserController::class);
+    Route::resource('barang', BarangController::class);
+});
+
 Route::get('/', function () {
     return view('home');
 });
 
 Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
