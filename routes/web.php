3b82bc8eb584be927bf1610bb617347e44df9ae2
akmlrnyn <akmalranyan@gmail.com>
2024-01-26 15:03:10 +0700

    define routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\RuanganController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::resource('user', UserController::class);
     Route::resource('barang', BarangController::class);
+    Route::resource('ruangan', RuanganController::class);
 });
 
 Route::get('/', function () {
